20acf740f1ca2015dfcfdfac09be10256037868d
SkateAllDay <raymundomc0205@icloud.com>
2024-11-28 12:46:33 -0500

    Update Spotify_API_proj.py

--- Spotify_API_proj.py
+++ Spotify_API_proj.py
@@ -1,3 +1,8 @@
+# import requests: usually requests is used to make http requests however the spotify API handles that for me
+# spotipy is the main library that I used to interact with the spotify api
+# SpotifyOAuth is a way of authenticating credentials, good when trying to access private user data
+# SpotifyClientCredentials: originally used to access public artist data, but i swithced it up in order to acess more private data
+
 from pip._vendor import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -8,25 +13,33 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 
 # Set up the credentials
-CLIENT_ID = 'EDITED FOR PRIVACY'  # Replace with your actual client ID
-CLIENT_SECRET = 'EDITED FOR PRIVACY'  # Replace with your actual client secret
-REDIRECT_URI = 'http://localhost:8888/callback'
+CLIENT_ID = 'EDITED FOR PRIVACY'  # identifies my app to the spotify api
+CLIENT_SECRET = 'EDITED FOR PRIVACY'  # used to authenticate that it is in fact me trying to make requests to the api
+REDIRECT_URI = 'http://localhost:8888/callback' # Oauth feedback: where I will be redirected after trying to authenticate
 #----------------------------------------------------
 import os
 
 #----------------------------------------------------
+# Determines the permissions the potential playlist will have, right now I wanted it to make private playlists at the moment
+# My user which has been redacted
 SCOPE = 'playlist-modify-private'
-user = 'FitGoesHard'
+user = '###########'
 
 #----------------------------------------------------
 
 # Authenticate using client credentials
+# sp is used to authenticate credentials using: client_id, client_secret, redirect_uri, and scope
+# sp is an instance of the spotipy.Spotify class, object allows you to interact with the spotify api
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri = REDIRECT_URI, scope = SCOPE))
-
+# current_user() - method provided by spotipy library, sends a request to the spotify API, on current authenticated user and returns a
+# dictionary containing details on the user(such as ID,name, email, followers, country
+# id part retreives the user's id and stored in user
 user = sp.current_user()['id']
 #-------------------------------------------------------------------------
 
-
+# While True is the main loop that asks if you want to create a playlist if yes, then you will be prompted to create a name and description
+# for the newly created playlist and then these details will be stored in variable playlist which will user an instance of sp and the 
+# user_playlist_create function in order to create the function which has 4 parameters(user,name, public/private,description
 while True:
     user_request = input("Would you like to create and name a playlist? (y or n)")
     if(user_request == "y"):
@@ -34,26 +47,26 @@ while True:
         playlist_d = input("Enter in a playlist description: \n")
 
         playlist =sp.user_playlist_create(user=user, name = playlist_n, public = False, description = playlist_d)
-        p_id = playlist['id']
+        p_id = playlist['id'] # p_id stored the playlists id for the future, in order to add songs to it
         
-        print(f"Playlist {playlist_n} created succesfully...")
-        while True:
-            user_conf = input("would you like to add songs to the playlist?(Y or N)").strip().lower()
+        print(f"Playlist {playlist_n} created succesfully...") ## print statement to let you know that a playlist has been added succesfully
+        while True: ## while true loop in order to further continue in whether you would like to add songs or not
+            user_conf = input("would you like to add songs to the playlist?(Y or N)").strip().lower() # 
             if(user_conf == "y"):
                 while True:
-                    user_add = input("type the song you want add:(exit to stop)").strip()
-                    if user_add == "exit":
+                    user_add = input("type the song you want add:(exit to stop)").strip() # user asked to enter in a song name
+                    if user_add == "exit": # if exit then this while true loop stops and goes to user_conf loop
                         print("exiting the song addition process.")
                         break
 
 
-                    try:
-                        result = sp.search(q = user_add, limit = 5, type = 'track')
-                        if not result['tracks']['items']:
-                            print(f"No songs for {user_add} found, try again")
+                    try: # try block first wants to try the search function using authenticated credentials(sp)
+                        result = sp.search(q = user_add, limit = 5, type = 'track') # limit of 5 tracks to choose from
+                        if not result['tracks']['items']: # if no tracks found then print statement will happen and user will be asked to try again
+                            print(f"No songs for {user_add} found, try again") # print statement to confirm that no songs found
                             continue
 
-                        print(f"Found the following songs for '{user_add}':")
+                        print(f"Found the following songs for '{user_add}':") # if songs found then this will print
                         for i, track in enumerate(result['tracks']['items']):
                              print(f"{i + 1}. {track['name']} by {track['artists'][0]['name']}")
                             
@@ -63,26 +76,26 @@ while True:
                             print("exiting number that you wanted")
                             break
                         try:
-                            song_choice = int(song_choice) - 1
-                            if song_choice < 0 or song_choice >= len(result['tracks']['items']):
+                            song_choice = int(song_choice) - 1 # lists in python 0 indexed, therefore if i type in 3 im really asking for position 4
+                            if song_choice < 0 or song_choice >= len(result['tracks']['items']): # cant enter a number less than zero while also checking the length of list tracks
                                 print("Invalid Choice, try again")
                                 continue
                             track_uri = result['tracks']['items'][song_choice]['uri']  # Get the URI of the first result
-                            sp.user_playlist_add_tracks(user=user, playlist_id=p_id, tracks=[track_uri])
-                            print(f"Song '{result['tracks']['items'][song_choice]['name']}' has been added to the playlist!")
+                            sp.user_playlist_add_tracks(user=user, playlist_id=p_id, tracks=[track_uri])# uses the user_playlist_add_tracks in order to add tracks needs authentication
+                            print(f"Song '{result['tracks']['items'][song_choice]['name']}' has been added to the playlist!") # confirmation
                         except ValueError:
-                            print("Invalid input, enter in a valid number:")
+                            print("Invalid input, enter in a valid number:") #Exception handling
 
                     except Exception as e:
-                        print(f"error adding song '{user_add}' : {e}")
+                        print(f"error adding song '{user_add}' : {e}") #exception handling
                         continue
 
             elif user_conf == "n":
-                print("Understand, songs will not be added to the playlist...")
+                print("Understand, songs will not be added to the playlist...") # if you choose not to add songs to playlist
                 break
 
             else:
-                print("Invalid input. Please enter 'Y' to add songs or 'N' to stop.")
+                print("Invalid input. Please enter 'Y' to add songs or 'N' to stop.") 
 
     elif(user_request == "n"):
         print("No user playlist will be created. ")
@@ -114,3 +127,21 @@ lana = 'https://open.spotify.com/artist/00FQb4jTyendYWaN8pK0wa?si=qRS3Cy5JSf6Gtb
 # List of features I want to add
 # - Ability to search up an artist from input
 # - ability to create a playlist and add songs to it
+
+
+
+# FOR LINES 70 - 71:
+# for API class, this is a nested dictionary and the "artists" key is another dictionary which has a list as a value
+# "tracks" is a key, "items" is the value of that key which "items" itself is a key to a list containing another key/value, which in it of itself contains another key/value pairing
+# {
+    #"tracks": {
+        #"items": [
+            #{"name": "Song 1", "artists": [{"name": "Artist 1"}]},
+            #{"name": "Song 2", "artists": [{"name": "Artist 2"}]},
+            #{"name": "Song 3", "artists": [{"name": "Artist 3"}]},
+            # More tracks...
+        #]
+    #}
+#}
+
+#
