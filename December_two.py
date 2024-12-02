870ea0a59b77e5c657aaac7640a0c8d5282f8885
SkateAllDay <raymundomc0205@icloud.com>
2024-12-02 05:06:54 -0500

    Create December_two.py
    trying to code something every day

---
+++ December_two.py
@@ -0,0 +1,27 @@
+def Opium(b):
+    b = b.split(" ")
+    print(b)
+    
+
+
+# Opium("BULLETS FLYING HOMIXIDE GANG")
+
+
+def thoughts():
+    
+    thought_list = []
+    
+    for i in range(10):
+        e = input("write out your thoughts: ")
+        thought_list.append(e)
+
+    
+    for c in thought_list:
+        print(c)
+
+
+
+thoughts()
+
+
+# lesson on passing inputs into functions
